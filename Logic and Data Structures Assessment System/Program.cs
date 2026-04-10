034b88da22570d2b9d14e4478e75fa45c507244b
Choklitos21 <diego.mm09@hotmail.com>
2026-04-10 14:31:14 -0500

    update

--- Logic and Data Structures Assessment System/Program.cs
+++ Logic and Data Structures Assessment System/Program.cs
@@ -37,8 +37,10 @@ void StartMenu()
                 JoinStrings();
                 break;
             case "4":
+                Calculator();
                 break;
             case "5":
+                NumericalAnalysis();
                 break;
             case "6":
                 break;
@@ -61,6 +63,7 @@ void StartMenu()
     
 }
 
+// Level 1
 void AddingTwoNumbers()
 {
     int num1;
@@ -92,6 +95,7 @@ void AddingTwoNumbers()
     Thread.Sleep(3000); 
 }
 
+// Level 2
 void AverageGrade()
 {
     double num1;
@@ -146,6 +150,7 @@ void AverageGrade()
     Thread.Sleep(3000);
 }
 
+// Level 3
 void JoinStrings()
 {
     Console.Write("\nType your first name: ");
@@ -165,24 +170,133 @@ void JoinStrings()
     Thread.Sleep(3000);
 }
 
+// Level 4
 void Calculator()
 {
     double num1;
     double num2;
     
     Console.Write("\nSelect an operation\n| + (add)\n| - (subtract)\n| * (multiply)\n| / (divide)\n| Choose: ");
-    string? operation = Console.ReadLine();
+    string operation = Console.ReadLine() ?? "";
+    while (operation == "" || operation != "+" || operation != "-" || operation != "*" || operation != "/")
+    {
+        Console.Write("Type one of the available options: ");
+        operation = Console.ReadLine() ?? "";
+    }
     
     Console.Write("\nType the first number: ");
-    string? input1 = Console.ReadLine();
+    while (!double.TryParse(Console.ReadLine(), out num1))
+    {
+        Console.Write("Type only numbers: ");
+    }
     
     Console.Write("\nType the second number: ");
-    string? input2 = Console.ReadLine();
-
-    if (double.TryParse(input1, out double out1))
+    while (!double.TryParse(Console.ReadLine(), out num2))
     {
-        
+        Console.Write("Type only numbers: ");
     }
+
+    switch (operation)
+    {
+        case "+":
+            Console.WriteLine($"The result of {num1} + {num2} = {num1 + num2}");
+            break;
+        case "-":
+            Console.WriteLine($"The result of {num1} - {num2} = {num1 - num2}");
+            break;
+        case "*":
+            Console.WriteLine($"The result of {num1} * {num2} = {num1 * num2}");
+            break;
+        case "/":
+            if (num1 == 0 || num2 == 0)
+            {
+                Console.WriteLine("Cannot divide by 0");
+                return;
+            }
+            Console.WriteLine($"The result of {num1} / {num2} = {num1 / num2}");
+            break;
+        default:
+            Console.WriteLine("Error");
+            break;
+    }
+    
+    
+}
+
+// Level 5
+void NumericalAnalysis()
+{
+    int count = 0;
+    List<double> numbersList = new List<double>();
+    
+    while (count < 5)
+    {
+        double num;
+        Console.Write($"\n#{count + 1} Type a number: ");
+        while (!double.TryParse(Console.ReadLine(), out num))
+        {
+            Console.Write("Type only numbers: ");
+        }
+        numbersList.Add(num);
+        count++;
+    }
+
+    Console.WriteLine($"The higher numbers is {numbersList.Max()}");
+    Console.WriteLine($"The lower numbers is {numbersList.Min()}");
+    Console.WriteLine($"The sum off all numbers is {numbersList.Sum()}");
+}
+
+// Level 6
+void TaskManagement()
+{
+    const string taskMenu = @"
+    --- Technical Training Center ---
+
+    Level 1: List all tasks
+    Level 2: Add a new Task
+    Level 3: Delete a Task by ID
+    Option 0: Exit Task Management menu
+
+    Select an option: ";
+
+    List<string> taskList = new List<string>();
+
+    bool taskFlag = true;
+    while (taskFlag == true)
+    {
+        Console.WriteLine(taskMenu);
+        string taskOption = Console.ReadLine() ?? "";
+        switch (taskOption)
+        {
+            case "1":
+                foreach (var task in taskList)
+                {
+                    Console.WriteLine($"\n***Task***\nDescription: {task}");
+                }
+                break;
+            case "2":
+                Console.WriteLine("Type the description for the new task: ");
+                string newTask = Console.ReadLine() ?? "";
+                while ( newTask == "")
+                {
+                    Console.WriteLine("Cannot be empty: ");
+                    newTask = Console.ReadLine() ?? "";
+                }
+                break;
+            case "3":
+                Console.WriteLine("");
+                break;
+            case "0":
+                Console.WriteLine("Out from Task Management");
+                taskFlag = false;
+                break;
+            default:
+                Console.WriteLine("Option not valid, try again");
+                break;
+        }
+    }
+
+
 }
 
 StartMenu();
